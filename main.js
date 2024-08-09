8bd3ac1ed83b8a9b14e7490321ec63417aa462b8
InfinityCity18 <kubawlas1@gmail.com>
2024-08-09 09:15:15 +0200

    second commit

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
-
 const width = 125;
 const height = 125;
 const scale = 100;
+const x_limit = 1000;
+const y_limit = 1000;
+const diff_accuracy = 0.1;
+const tangents_diff = 1;
 
-setDocDimensions(width, height);
-
-const diff_accuracy = 0.002;
-
-var function_points = []
-
-for (let i = -width; i <= width; i += diff_accuracy) {
-  function_points.push([i, f(i)]);
-}
-
-const inverse = false;
-
-//define your own function and also provide its derivative
 function f(x) {
-  return x * x * x
+  return x * x * x;
 }
 
 function df(x) {
   return 3 * x * x;
 }
+
+function get_tangent_points(a) {
+  
+}
+
+function get_y_from_x_tangent(x, a) {
+  return df(a)*(x-a) + f(a)
+}
+
+function get_x_from_y_tangent(y, a) {
+  return (y+a*df(a)-f(a))/(df(a));
+}
+
+setDocDimensions(width, height);
+
+var function_points = []
+var tangents_x = []
+
+for (let i = -width; i <= width; i += diff_accuracy) {
+  function_points.push([i, f(i)]);
+}
+
+for (let i = -width; i <= width; i += tangents_diff) {
+  tangents_x.push(i);
+}
+
+const inverse = false;
+
+//define your own function and also provide its derivative
+
 
 const finalLines = [];
 
